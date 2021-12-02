8a8a3886d488ee59f6ea64b0296b852259c31714
OfficialLFSelepe <lesetjaofficial26@gmail.com>
2021-12-02 14:00:03 +0200

    Implemented Register identity

--- uApply.Data/Models/ApplicationUser.cs
+++ uApply.Data/Models/ApplicationUser.cs
@@ -9,6 +9,22 @@ namespace uApply.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FullNames { get; set; }
+        public string Surname { get; set; }
+        public string Title { get; set; }
+        public string Gender { get; set; }
+        public string Nationality { get; set; }
+        public long IdNumber{ get; set; }
+        public string Race { get; set; }
+        public string HomeLanguage { get; set; }
+        public string StreetAddress { get; set; }
+        public string Surburb { get; set; }
+        public int PostalCode { get; set; }
+        public string Town { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password{ get; set; }
+
         public string Role { get; set; }
     }
 }
--- uApply.Utils/ViewModels/UserVM/UserViewModel.cs
+++ uApply.Utils/ViewModels/UserVM/UserViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using uApply.Data.Models;
 
 namespace uApply.Utils.ViewModels.UserVM
 {
     public class UserViewModel
     {
+        public User User { get; set; }
+        public IEnumerable<SelectListItem> Titles { get; set; }
+        public IEnumerable<SelectListItem> Genders { get; set; }
+        public IEnumerable<SelectListItem> Races { get; set; }
+        public IEnumerable<SelectListItem> Nationalities { get; set; }
+        public IEnumerable<SelectListItem> Languages { get; set; }
+        public IEnumerable<Learner> Learners { get; set; }
+
     }
 }
